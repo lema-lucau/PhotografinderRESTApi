e4cf9e00035ff24305d289fa216b266cc37ad245
Lema Lucau <55754825+lema-lucau@users.noreply.github.com>
2022-03-13 23:20:38 +0000

    Complete Users API

--- routes/photoshoots.js
+++ routes/photoshoots.js
@@ -1,47 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.send("We are on photoshoots");
 });
 
 // Add a new photoshoot
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
     res.send("Add a new photoshoot");
 });
 
 // Amend a photoshoot's details
-router.put("/amend/:photoshootId", (req, res) => {
+router.put("/amend/:photoshootId", async (req, res) => {
     res.send("Amend a photoshoot's details");
 });
 
 // Delete a photoshoot
-router.delete("/delete/:photoshootId", (req, res) => {
+router.delete("/delete/:photoshootId", async (req, res) => {
     res.send("Delete a photoshoot");
 });
 
 // Update the status of a photoshoot
-router.put("/update-status/:photoshootId", (req, res) => {
+router.patch("/update-status/:photoshootId", async (req, res) => {
     res.send("Update the status of a photoshoot");
 });
 
 // Fetch all the user's photoshoots
-router.get("/retrieve", (req, res) => {
+router.get("/retrieve", async (req, res) => {
     res.send("Fetch all the user's photoshoots");
 });
 
 // Retrieve a photoshoot by it's date and start time
-router.get("/retrieve/by-date-start-time", (req, res) => {
+router.get("/retrieve/by-date-start-time", async (req, res) => {
     res.send("Retrieve a photoshoot by it's date and start time");
 });
 
 // Retrieve a photoshoot by it's status
-router.get("/retrieve/by-status", (req, res) => {
+router.get("/retrieve/by-status", async (req, res) => {
     res.send("Retrieve a photoshoot by it's status");
 });
 
 // Return a photoshoot by it's ID
-router.get("/retrieve/:photoshootId", (req, res) => {
+router.get("/retrieve/:photoshootId", async (req, res) => {
     res.send("Return a photoshoot by it's ID");
 });
 
