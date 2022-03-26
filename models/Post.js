4e514ab8defeeb13d77546f2cc4e16f7c2b5b027
Lema Lucau <55754825+lema-lucau@users.noreply.github.com>
2022-03-26 20:48:22 +0000

    Add ownerUsername and imageName to posts
    - Sort by timestamp (desc) when fetching photographers posts

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const PostSchema = mongoose.Schema({
     id: { type: String, required: true },
     ownerId: { type: String, required: true },
+    ownerUsername: { type: String, required: true },
     imageUrl: { type: String, required: true },
+    imageName: { type: String, required: true },
     likes: { type: [String], default: [] },
     timestamp: { type: Number, default: Date.now },
 });
